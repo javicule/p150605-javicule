8bada5624875a6947a18fc907a8210ebbe4fcfa1
javicule <javicule93@gmail.com>
2015-06-05 10:16:56 +0200

    Tras documentar Javadoc Fco Javi Jiménez Paredes

--- P20150605/src/p20150605/AudioSpot.java
+++ P20150605/src/p20150605/AudioSpot.java
@@ -2,6 +2,15 @@ package p20150605;
 
 import java.io.File;
 
+/**
+ * Representa anuncios publicitarios en la radio.
+ * 
+ * Además de almacenar datos sobre el anuncio y su audio, permite poner el anuncio sobre una secuencia de clips y
+ * exportarlo todo en formato daf.
+ * 
+ * @author Francisco Javier Jiménez Paredes
+ * @version 1.0.1
+ */
 public class AudioSpot
 {
     private File archivo;       // manejador para el archivo que contiene el audio (.mp3)
@@ -9,8 +18,18 @@ public class AudioSpot
     private String producto;    // nombre del producto anunciado
     private String anunciante;  // nombre de la empresa anunciante
     
+    /**
+     * Contiene mensajes de error diferentes, que se corresponden con los valores devueltos por algunos métodos.
+     * (-1) No se ha establecido duración alguna.
+     * (-2) No se ha indicado el nombre del producto anunciado.
+     * (-3) No se ha indicado el nombre de la empresa anunciante.
+     * (-4) No se ha establecido el archivo de audio.
+     */
     public String lasterrormsg;
     
+    /**
+     * Establece los valores iniciales de un anuncio vacío.
+     */
     public AudioSpot ()
     {
         this.archivo = null;
@@ -20,12 +39,22 @@ public class AudioSpot
         this.lasterrormsg = "";
     }
     
+    /**
+     * Establece los metadatos del anuncio.
+     * @param producto Nombre que recibirá el producto en venta.
+     * @param anunciante Nombre de la empresa o persona que vende el producto.
+     */
     public void setMetaDatos (String producto, String anunciante)
     {
         this.producto = producto;
         this.anunciante = anunciante;
     }
     
+    /**
+     * Establece la duración que tendrá el anuncio.
+     * @param duracion Tiempo en segundos que durará el anuncio.
+     * @throws IllegalArgumentException Cuando el tiempo del anuncio es mayor de 120 segundos. 
+     */
     public void setDuracion(int duracion) throws IllegalArgumentException
     {
         if (duracion<0)
@@ -35,12 +64,22 @@ public class AudioSpot
         this.duracion = duracion;
     }
     
+    /**
+     * Vincula el archivo de audio al anuncio.
+     * @param nombre_archivo Ruta absoluta o relativa al fichero de audio.
+     * @return True, si el archivo existe, false si no existe.
+     */
     public Boolean setArchivo(String nombre_archivo)
     {
         this.archivo = new File(nombre_archivo);
         return this.archivo.exists();
     }
     
+    /**
+     * Coloca el anuncio en la cola de anuncios que aparecerá en la radio.
+     * @param cola_reproduccion Cola de anuncios donde poner el nuevo anuncio.
+     * @return Código que indica si todo ha salido bien o si ha habido algún problema, 0 - Éxito, -1 - Falta duración, -2 - Falta nombre del producto, -3 - Falta nombre de la empresa, -4 - Falta archivo de audio, -5 - Fallo al añadirlo a la cola.
+     */
     public int ProgramaEnCola(Object cola_reproduccion)
     {
         // comprobamos previamente que no falte nada
@@ -85,6 +124,11 @@ public class AudioSpot
             return resultado;
     }
     
+    /**
+     * Convierte el anuncio y el audio a un archivo DAW.
+     * @param objeto_daw Archivo DAW donde se exportará el anuncio.
+     * @return Código que indica si todo ha salido bien o si ha habido algún problema, 0 - Éxito, -1 - Falta duración, -2 - Falta nombre del producto, -3 - Falta nombre de la empresa, -4 - Falta archivo de audio, -5 - Fallo al añadirlo a la cola.
+     */
     public int ExportaAFormatoDAW(Object objeto_daw)
     {
         // comprobamos previamente que no falte nada
